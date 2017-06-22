521559cae351a6d1d009d8d5dca9ad9869129180
BuhMaga <g8.liver@gmail.com>
2017-06-23 00:35:13 +0300

    zzx

---
+++ Game/Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class test : MonoBehaviour
+{
+
+    // Use this for initialization
+
+
+
+    // Update is called once per frame
+    public void FirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void SecondScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
+    
+
